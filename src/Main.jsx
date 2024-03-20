8014be76c40b4709032bdfecb581e97b4154cc7e
Roy Ledford <bluesixty@gmail.com>
2024-03-20 11:27:32 -0400

    cleanup

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,9 @@
-import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import App from './App'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools  />
-    </QueryClientProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,4 @@
-import { Link } from 'react-router-dom'
-import { useAuth } from '../../providers/AuthProvider'
-import PostsApi from '@services/posts'
-import useQuery from '../../hooks/useQuery'
-
 export default function Home() {
-  const { token, removeToken } = useAuth()
-
-  const { data, isLoading } = useQuery(['posts'], () => PostsApi.all())
-  console.log('data', data) // TODO: remove this
-
   return (
     <div
       style={{
@@ -18,11 +8,6 @@ export default function Home() {
       }}
     >
       <h1>Home Page</h1>
-      {token ? (
-        <button onClick={removeToken}>Logout</button>
-      ) : (
-        <Link to='/login'>Login</Link>
-      )}
     </div>
   )
 }
